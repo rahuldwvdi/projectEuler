6f8d5a3e4f87b9afc077a37fe3775c417f80e167
rahuldwvdi <51382525+rahuldwvdi@users.noreply.github.com>
2020-10-17 09:51:35 +0530

    Update specialpythagoreantriplet.py

--- specialpythagoreantriplet.py
+++ specialpythagoreantriplet.py
@@ -1,3 +1,9 @@
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#a2 + b2 = c2
+#For example, 32 + 42 = 9 + 16 = 25 = 52.
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
+
 for i in range(3, 1000):
     for j in range (i + 1, 1000):
         k = i**2 + j**2
